80aace7a42407655f965f5ba6953ca3ee6e76d0f
kiruikev <87135895+kiruikev@users.noreply.github.com>
2021-08-05 14:35:38 +0300

    Update ft-reviews.js
    modified

--- Assets/JS/ft-reviews.js
+++ Assets/JS/ft-reviews.js
@@ -1,48 +1,8 @@
+$("document").ready(function () {
+  $("#button").click(function (e) {
+    $(".hide").toggle();
+    $("#button").hide();
+  });
+  e.preventDefault();
+});
 
-$("document").ready(function(){
-    $("#button").click(function (e) { 
-        $(".hide").show();
-                $("#button").click(function(){
-                    $(".hide").hide();
-                        $("#button").click(function(){
-                            $(".hide").show();
-                            $("#button").click(function(){
-                                $(".hide").hide();
-                                    $("#button").click(function(){
-                                        $(".hide").show();
-                                    })
-                                    $("#button").click(function(){
-                                        $(".hide").hide();
-                                            $("#button").click(function(){
-                                                $(".hide").show();
-                                            })
-                                            $("#button").click(function(){
-                                                $(".hide").hide();
-                                                    $("#button").click(function(){
-                                                        $(".hide").show();
-                                                    })
-                                                    $("#button").click(function(){
-                                                        $(".hide").hide();
-                                                            $("#button").click(function(){
-                                                                $(".hide").show();
-                                                            })
-                                                        })
-                                                    })
-                                                })
-                                            })
-                        })
-                })
-            });
-        e.preventDefault();
-        
-    });
-
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
